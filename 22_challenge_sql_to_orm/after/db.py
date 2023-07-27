c7094fa6f84aa4d9ab5e748e0d4c2e7ba2f467b2
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-27 17:08:26 +0200

    Add SQL to ORM solution

---
+++ 22_challenge_sql_to_orm/after/db.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class EventCreate(BaseModel):
+    title: str
+    location: str
+    start_date: datetime
+    end_date: datetime
+    available_tickets: int
+
+
+class TicketCreate(BaseModel):
+    event_id: int
+    customer_name: str
+    customer_email: str
